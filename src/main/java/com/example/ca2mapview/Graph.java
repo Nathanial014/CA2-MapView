827af6636e110bcf9a83a86724759f4f2024d558
Nathanial014 <20103232@mail.wit.ie>
2024-05-01 15:11:12 +0100

    Changes by Admin = Graph and RouteFinder created

--- src/main/java/com/example/ca2mapview/Graph.java
+++ src/main/java/com/example/ca2mapview/Graph.java
@@ -9,14 +9,27 @@ public class Graph {
     private Map<String, Node> nodes = new HashMap<>();
     private List<Edge> edges = new ArrayList<>();
 
+    //public void addNode(Node node) {
+   //     nodes.put(node.getId(), node);
+    //}
+
     public void addNode(Node node) {
         nodes.put(node.getId(), node);
+        adjacencyList.put(node, new ArrayList<>());  // Initialize adjacency list for the new node
     }
 
     public void addEdge(Node start, Node end, double distance) {
         Edge edge = new Edge(start, end, distance);
         edges.add(edge);
+        adjacencyList.get(start).add(edge);  // Add the edge to the start node's list
     }
+
+
+
+    // void addEdge(Node start, Node end, double distance) {
+    //    Edge edge = new Edge(start, end, distance);
+    //    edges.add(edge);
+    //}
 
     public Node getNode(String id) {
         return nodes.get(id);
@@ -29,5 +42,15 @@ public class Graph {
     public List<Edge> getEdges() {
         return edges;
     }
+    // Method to get all edges from a given node
+    public List<Edge> getEdgesFromNode(Node current) {
+        return adjacencyList.getOrDefault(current, new ArrayList<>());
+    }
+
+    private Map<Node, List<Edge>> adjacencyList = new HashMap<>();
+
+
 }
+
+
 
--- src/main/java/com/example/ca2mapview/RouteFinder.java
+++ src/main/java/com/example/ca2mapview/RouteFinder.java
@@ -1,5 +1,8 @@
 package com.example.ca2mapview;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class RouteFinder {
     private Graph graph;
 
@@ -7,19 +10,27 @@ public class RouteFinder {
         this.graph = graph;
     }
 
-    public List<Node> findShortestRoute(Node start, Node end) {
+    // Find the shortest route by distance using Dijkstra's algorithm
+    public List<Node> findShortestRouteByDistance(Node start, Node end) {
         Dijkstra dijkstra = new Dijkstra(graph);
         return dijkstra.findShortestPath(start, end);
     }
 
-    public List<Node> findBFSRoute(Node start, Node end) {
+    // Find the shortest route in terms of hops using BFS
+    public List<Node> findShortestRouteByHops(Node start, Node end) {
         BFS bfs = new BFS(graph);
         return bfs.findRoute(start, end);
     }
 
-    public List<List<Node>> findDFSPermutations(Node start, Node end, int maxRoutes) {
+    // Find multiple route permutations using DFS
+    public List<List<Node>> findRoutePermutations(Node start, Node end, int maxRoutes) {
         DFS dfs = new DFS(graph, maxRoutes);
         return dfs.findRoutes(start, end);
     }
-}
 
+    // Find the most cultural or historic route using a modified Dijkstra's algorithm
+    public List<Node> findMostCulturalRoute(Node start, Node end) {
+        Dijkstra dijkstra = new Dijkstra(graph);
+        return dijkstra.findMostCulturalPath(start, end);
+    }
+}
